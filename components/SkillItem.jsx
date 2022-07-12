74535e2b1706404cfe16fb319ea477b214894660
MacBook de Mamady <KONTE@mac-mamady.local>
2022-07-12 23:49:02 +0200

    v1

--- components/SkillItem.jsx
+++ components/SkillItem.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const SkillItem = ({ name, icon }) => {
   return (
     <div className="flex flex-col">
-      <h3>{name}</h3>
       {icon}
+      <h3>{name}</h3>
     </div>
   );
 };
